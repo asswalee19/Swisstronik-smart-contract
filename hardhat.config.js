9183bb0e864b512e7b1dcf827f3dd45425e31bb4
asswalee19 <asswalee19@gmail.com>
2024-07-24 14:32:35 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0xaf892cee9b731abe0d0ba81338ab7f8d87e5eecf3e071f1a1a1967fafc42174f"], //Your private key starting with "0x" 
+      accounts: ["0x......"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
